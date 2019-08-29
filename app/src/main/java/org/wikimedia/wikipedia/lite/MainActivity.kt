88ed2d1f32a4ad69e18e375f48048ec836fc837a
Joe Walsh <joewalsh@users.noreply.github.com>
2019-08-29 15:27:58 -0400

    initial commit

---
+++ app/src/main/java/org/wikimedia/wikipedia/lite/MainActivity.kt
@@ -0,0 +1,31 @@
+package org.wikimedia.wikipedia.lite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.ValueCallback
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MyClient: WebViewClient() {
+    override fun onPageFinished(view: WebView?, url: String?) {
+        super.onPageFinished(view, url)
+    }
+}
+class MainActivity : AppCompatActivity() {
+    var client = MyClient()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webView.loadUrl("http://192.168.1.26:6927/en.wikipedia.org/v1/page/mobile-html-shell/")
+        webView.webViewClient = client
+        loadButton.setOnClickListener {
+            var js = "pagelib.c1.Page.load('http://192.168.1.26:6927/en.wikipedia.org/v1/page/mobile-html/${titleEditText.text}');"
+            webView.evaluateJavascript(js,
+            ValueCallback<String> {
+
+            })
+        }
+    }
+
+}
