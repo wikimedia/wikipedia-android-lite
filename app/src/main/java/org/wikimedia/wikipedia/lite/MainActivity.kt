5bcbbd8bcfbfcf2ca9ad120c2bc4b39c92777f8d
Joe Walsh <joewalsh@users.noreply.github.com>
2019-08-29 17:23:32 -0400

    add timer

--- app/src/main/java/org/wikimedia/wikipedia/lite/MainActivity.kt
+++ app/src/main/java/org/wikimedia/wikipedia/lite/MainActivity.kt
@@ -2,30 +2,81 @@ package org.wikimedia.wikipedia.lite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.webkit.JavascriptInterface
 import android.webkit.ValueCallback
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MyClient: WebViewClient() {
+class Client: WebViewClient() {
+    var incomingMessageHandler: ValueCallback<String>? = null
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
+        view?.evaluateJavascript("pagelib.c1.Page.setup({" +
+                "platform: pagelib.c1.Platforms.ANDROID," +
+                "clientVersion: '0.0.0'," +
+                "theme: pagelib.c1.Themes.DARK," +
+                "dimImages: false," +
+                "margins: { top: '16px', right: '16px', bottom: '16px', left: '16px' }," +
+                "areTablesInitiallyExpanded: false," +
+                "textSizeAdjustmentPercentage: '100%%'," +
+                "loadImages: true" +
+                "}," +
+                "() => { marshaller.onReceiveMessage('{\"action\": \"setup_complete\"}') }" +
+                ")",
+            ValueCallback<String> {
+
+            })
+    }
+
+    override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+        super.onPageStarted(view, url, favicon)
+    }
+
+    fun loadTitle(title: String, webView: WebView) {
+        var js = "pagelib.c1.Page.load('https://en.wikipedia.org/api/rest_v1/page/mobile-html/${title}').then(() => { marshaller.onReceiveMessage('{\"action\": \"load_complete\"}') } )"
+        webView.evaluateJavascript(js,
+            ValueCallback<String> {
+
+            })
+    }
+
+    @JavascriptInterface
+    fun onReceiveMessage(message: String) {
+        incomingMessageHandler?.onReceiveValue(message)
     }
 }
+
 class MainActivity : AppCompatActivity() {
-    var client = MyClient()
+    var client = Client()
+    var startTime: Long = 0
+    var endTime: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         webView.loadUrl("http://192.168.1.26:6927/en.wikipedia.org/v1/page/mobile-html-shell/")
         webView.settings.javaScriptEnabled = true
-        webView.webViewClient = client
-        loadButton.setOnClickListener {
-            var js = "pagelib.c1.Page.load('http://192.168.1.26:6927/en.wikipedia.org/v1/page/mobile-html/${titleEditText.text}');"
-            webView.evaluateJavascript(js,
-            ValueCallback<String> {
+        client.incomingMessageHandler = ValueCallback<String> {
+            try {
+                val messagePack = JSONObject(it)
+                var action = messagePack.get("action")
+                if (action == "setup_complete") {
 
-            })
+                } else if (action == "load_complete") {
+                    endTime = System.currentTimeMillis()
+                    timeTextView.text = "${endTime - startTime}"
+                }
+            } catch (e: JSONException) {
+                throw RuntimeException(e)
+            }
+        }
+        webView.webViewClient = client
+        webView.addJavascriptInterface(client, "marshaller")
+        loadButton.setOnClickListener {
+            startTime = System.currentTimeMillis()
+            client.loadTitle(titleEditText.text.toString(), webView)
         }
     }
 
